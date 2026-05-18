bce1bec8ef845832b4861a633e14bb9411dfe0f4
Aniket Singh <anikets101@gmail.com>
2026-05-18 21:28:52 +0530

    Added some features to Parser

--- lexer.c
+++ lexer.c
@@ -9,7 +9,6 @@ TokenType checkKeyword(char* word) {
     if(strcmp(word, "SELECT") == 0) {
         return TOKEN_SELECT;
     }
-
     if(strcmp(word, "FROM") == 0) {
         return TOKEN_FROM;
     }
@@ -33,7 +32,6 @@ int tokenize(char* input, Token tokens[]) {
             i++;
             continue;
         }
-
         // WORDS
         if(isalpha(input[i])) {
 
@@ -41,21 +39,15 @@ int tokenize(char* input, Token tokens[]) {
             int j = 0;
 
             while(isalpha(input[i])) {
-
                 buffer[j] = input[i];
-
                 j++;
                 i++;
             }
 
             buffer[j] = '\0';
-
             Token token;
-
             token.type = checkKeyword(buffer);
-
             strcpy(token.value, buffer);
-
             tokens[tokenCount++] = token;
         }
 
@@ -68,32 +60,20 @@ int tokenize(char* input, Token tokens[]) {
         }
         // *
         else if(input[i] == '*') {
-
             Token token;
-
             token.type = TOKEN_STAR;
-
             strcpy(token.value, "*");
-
             tokens[tokenCount++] = token;
-
             i++;
         }
-
         // ;
         else if(input[i] == ';') {
-
             Token token;
-
             token.type = TOKEN_SEMICOLON;
-
             strcpy(token.value, ";");
-
             tokens[tokenCount++] = token;
-
             i++;
         }
-
         else {
 
             printf("Unexpected character: %c\n", input[i]);
--- parser.c
+++ parser.c
@@ -3,67 +3,61 @@
 
 #include "parser.h"
 
-void syntaxError() {
+void syntaxError()
+{
 
-    printf("Syntax Error!\n");
+    printf("Syntax Error! \n");
 }
 
-void parse(Token tokens[], int tokenCount) {
-
+void parse(Token tokens[], int tokenCount)
+{
     int current = 0;
-
     SelectStatement stmt;
-
     // SELECT
-    if(tokens[current].type != TOKEN_SELECT) {
-
+    if (tokens[current].type != TOKEN_SELECT)
+    {
         syntaxError();
         return;
     }
-
     current++;
-
     // COLUMN
-    if(tokens[current].type == TOKEN_IDENTIFIER ||
-       tokens[current].type == TOKEN_STAR) {
-
+    if (tokens[current].type == TOKEN_IDENTIFIER ||
+        tokens[current].type == TOKEN_STAR)
+    {
         strcpy(stmt.column, tokens[current].value);
     }
-    else {
+    else
+    {
 
         syntaxError();
         return;
     }
-
     current++;
-
     // FROM
-    if(tokens[current].type != TOKEN_FROM) {
-
+    if (tokens[current].type != TOKEN_FROM)
+    {
         syntaxError();
         return;
     }
-
     current++;
-
     // TABLE NAME
-    if(tokens[current].type != TOKEN_IDENTIFIER) {
-
+    if (tokens[current].type != TOKEN_IDENTIFIER)
+    {
         syntaxError();
         return;
     }
-
     strcpy(stmt.table, tokens[current].value);
-
     current++;
-
-    // ;
-    if(tokens[current].type != TOKEN_SEMICOLON) {
-
-        syntaxError();
-        return;
-    }
-
+    // if (current >= tokenCount)
+    // {
+    //     syntaxError();
+    //     return;
+    // }
+    // if (tokens[current].type != TOKEN_SEMICOLON)
+    // {
+    //     syntaxError();
+    //     return;
+    // }
     printf("Parsed SELECT Statement\n");
     printf("Column: %s\n", stmt.column);
     printf("Table : %s\n", stmt.table);
